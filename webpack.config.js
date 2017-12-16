5930219fad9ccd8901ea6656d9bc4c895502bf7d
wujiank <wujiank@yonyou.com>
2017-12-16 15:09:37 +0800

    修改热替换模块问题

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports={
-	// devtool:'eval-source-map',//便于开发环境找错误位置，eval-source-map只用于开发环境
+	devtool:'eval-source-map',//便于开发环境找错误位置，eval-source-map只用于开发环境
 	entry:__dirname+"/app/index.js",//唯一的入口文件
 	output:{
 		path:__dirname+"/public",//产出文件所在的位置
@@ -40,11 +40,10 @@ module.exports={
 		]
 
 	},
-	plugins:[new HtmlWebpackPlugin(
-		{
-			 template: __dirname + "/app/index.tmpl.html"//生产index.html模版文件
-		},
-		new webpack.HotModuleReplacementPlugin()
-	)]
+	plugins:[new HtmlWebpackPlugin({
+		    template: __dirname + "/app/index.tmpl.html"//生产index.html模版文件
+		}),  
+		/* 在开发环境中配置通过配置HMR 以提高开发效率*/
+		  new webpack.HotModuleReplacementPlugin()]
 }
 /*__dirname”是node.js中的一个全局变量，它指向当前执行脚本所在的目录。*/
